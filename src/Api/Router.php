847e9897360b56c0d24a7f31f54d305900d427f6
alpfish <254997790@qq.com>
2016-07-22 22:53:06 +0800

    添加 Api 注释文档安装及生成方法

--- src/Api/Router.php
+++ src/Api/Router.php
@@ -13,7 +13,7 @@ class Router
         //Api 根目录
         if( !file_exists($api_path)) exit('Api 根目录不存在！');
         //Api 版本
-        $v = request('v') ? request('v') : 'V1';
+        $v = request('v') ? request('v') : 'v1';
         if( !file_exists($api_path .= $v . '/')) exit('$v： 版本不正确');
         //Api 名称
         if( !$method = request('method')) exit('$method： 参数不存在');
@@ -27,11 +27,12 @@ class Router
         
         $controller = $method[$count - 2];
         $action = $method[$count - 1];
-        $file = $api_path . $controller . $api_ext;
-
-        if (is_file($file) && require_cache($file) && class_exists($controller)  && method_exists($controller, $action)) {
-            $class = new $controller;
-            return $class->$action();
+        if(is_file($file = $api_path . $controller . $api_ext)) {
+            require_once $file;
+            if(class_exists($controller)  && method_exists($controller, $action)) {
+                $class = new $controller;
+                return $class->$action();
+            }
         }
 
         exit('$method： Api 不存在');
